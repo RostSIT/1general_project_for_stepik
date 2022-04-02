41d9b3385a58b590633cf6e703878b37e26eaf68
RostSIT <Git2020Rost>
2022-04-02 17:16:24 +0300

    Добавлены: login_page.py с новыми методами тестирования стр., locators.py для удобства поддержки тестов (все локаторы в одном файле)

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,12 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
-import time
+from .locators import MainPageLocators
+
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        time.sleep(3)
-        login_link.click()
-        time.sleep(3)
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Login link is not presented'
