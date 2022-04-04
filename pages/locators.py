701cc628ee2166d4f78ecd77a94420feb321e749
RostSIT <Git2020Rost>
2022-04-04 20:49:37 +0300

    Добавлены: новые тесты в test_product_page.py, test_main_page.py добавлен конструктор __init__

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,17 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+
+class Basket:
+    CART_BUTTON = (By.CSS_SELECTOR, '#add_to_basket_form > button')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(2) > div')
+    ADD_NAME = (By.CSS_SELECTOR, 'article>div:nth-child(1)>div:nth-child(2) h1')
+    IN_NAME = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div > strong')
+    ADD_PRICE = (By.CSS_SELECTOR, 'article>div:nth-child(1)>div:nth-child(2) p.price_color')
+    IN_PRICE = (By.CSS_SELECTOR, '#messages > div:nth-child(3) > div  strong')
+
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
---
+++ pages/product_page.py
@@ -0,0 +1,49 @@
+from .base_page import BasePage
+from .locators import Basket
+
+
+class ProductPage(BasePage):
+    def __init__(self, *args, **kwargs):
+        super(ProductPage, self).__init__(*args, **kwargs)
+
+
+class PageObject(BasePage):
+
+    def add_product_to_basket(self):
+        self.should_be_added_book()
+        self.should_be_checked_name_book()
+        self.should_be_checked_price_book()
+
+    def open_click_solve(self):
+        self.open()
+        self.click_button()
+        self.solve_quiz_and_get_code()
+
+    def click_button(self):
+        self.cart_button_click(*Basket.CART_BUTTON)
+
+    def should_be_added_book(self):
+        self.is_element_present(*Basket.SUCCESS_MESSAGE)
+
+    def should_be_checked_name_book(self):
+        add_name = self.browser.find_element(*Basket.ADD_NAME)
+        in_name = self.browser.find_element(*Basket.IN_NAME)
+        print(add_name, in_name)
+        assert add_name.text == in_name.text, 'Different names of added and claimed books'
+
+    def should_be_checked_price_book(self):
+        add_price = self.browser.find_element(*Basket.ADD_PRICE)
+        in_price = self.browser.find_element(*Basket.IN_PRICE)
+        assert add_price.text == in_price.text, 'Different prices'
+
+    def guest_cant_see_success_message_after_adding_product_to_basket(self):
+        # assert self.is_element_present(*Basket.SUCCESS_MESSAGE), "1The success message is not presented."
+        assert self.is_not_element_present(*Basket.SUCCESS_MESSAGE), "1Success message is presented."
+
+    def guest_cant_see_success_message(self):
+        # assert self.is_element_present(*Basket.SUCCESS_MESSAGE), "2The success message is not presented."
+        assert self.is_not_element_present(*Basket.SUCCESS_MESSAGE), "2Success message is presented."
+
+    def message_disappeared_after_adding_product_to_basket(self):
+        # assert self.is_element_present(*Basket.SUCCESS_MESSAGE), "3The success message is not presented."
+        assert self.is_disappeared(*Basket.SUCCESS_MESSAGE), "3Success message is presented."
